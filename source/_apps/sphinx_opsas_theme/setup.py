729fb3c38f3afb3c1c35cf82c863442d919366d4
calmzhu <saint@justcalm.org>
2020-05-31 20:37:10 +0800

    Start with sphinx and add custom theme

---
+++ source/_apps/sphinx_opsas_theme/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    entry_points={
+        'sphinx.html_themes': [
+            'sphinx_opsas_theme = sphinx_opsas_theme',
+        ]
+    },
+)
---
+++ source/index.rst
@@ -0,0 +1,13 @@
+点滴之间
+============
+
+
+诫言
+-------
+.. toctree::
+  :maxdepth: 2
+  :numbered:
+
+  slogan.rst
+
+
